e91379aea8472cba1431a4c67ebc3a76f82d5266
drabinowitz <dmitrirabinowitz+code@gmail.com>
2015-01-31 00:16:46 -0500

    Working on nonnested implementation.

--- src/__tests__/r-test.js
+++ src/__tests__/r-test.js
@@ -1,39 +1,102 @@
 jest.dontMock('../r');
 
 describe('r', function () {
-  var r, React, mockProps;
+  var r, React, mockProps, mockReturn, mockReactClass;
+
   beforeEach(function () {
     r = require('../r');
     React = require('react/addons');
+    mockReactClass = React.createClass({
+      render: function () {
+        return React.createElement('div', null);
+      }
+    });
     React.createElement = jest.genMockFunction();
     mockProps = {};
+    mockReturn = {};
   });
 
-  it('should properly invoke React.createElement when rendering a single tag', function () {
-    r('br');
+  it('should invoke React.createElement with a single element', function () {
+    r.compose(
+      r('br /')
+    );
+
     expect(React.createElement.mock.calls[0][0]).toBe('br');
     expect(React.createElement.mock.calls[0][1]).toBe(null);
   });
 
-  it('should handle self closing tags with our without spaces when rendering a single tag', function () {
-    r('br/');
-    expect(React.createElement.mock.calls[0][0]).toBe('br');
-    expect(React.createElement.mock.calls[0][1]).toBe(null);
-    r('br /');
-    expect(React.createElement.mock.calls[1][0]).toBe('br');
-    expect(React.createElement.mock.calls[1][1]).toBe(null);
-  });
+  it('should invoke React.createElement with an element with text', function () {
+    r,compose(
+      r('h1'), 'hello, world', r('/h1')
+    );
 
-  it('should allow a tag to render with text as its only child', function () {
-    r('h1', 'hello, world');
     expect(React.createElement.mock.calls[0][0]).toBe('h1');
     expect(React.createElement.mock.calls[0][1]).toBe(null);
     expect(React.createElement.mock.calls[0][2]).toBe('hello, world');
   });
 
-  it('should allow a tag to render with only props', function () {
-    r('div', mockProps);
+  it('should invoke React.createElement with an element with props', function () {
+    r.compose(
+      r('div', mockProps, '/')
+    );
+
     expect(React.createElement.mock.calls[0][0]).toBe('div');
     expect(React.createElement.mock.calls[0][1]).toBe(mockProps);
+  });
+
+  it('should invoke React.createElement with an element with text and props', function () {
+    r.compose(
+      r('p', mockProps), 'what cool syntax', r('/p')
+    );
+
+    expect(React.createElement.mock.calls[0][0]).toBe('p');
+    expect(React.createElement.mock.calls[0][1]).toBe(mockProps);
+    expect(React.createElement.mock.calls[0][2]).toBe('what cool syntax');
+  });
+
+  it('should invoke React.createElement with a custom element', function () {
+    r.compose(
+      r(mockReactClass, '/')
+    );
+    r.compose(
+      r(mockReactClass, mockProps, '/')
+    );
+    r.compose(
+      r(mockReactClass), 'inner child', r('/mockReactClass')
+    );
+    r.compose(
+      r(mockReactClass, mockProps), 'inner child', r('/mockReactClass')
+    );
+
+    expect(React.createElement.mock.calls[0][0]).toBe(mockReactClass);
+    expect(React.createElement.mock.calls[0][1]).toBe(null);
+
+    expect(React.createElement.mock.calls[0][0]).toBe(mockReactClass);
+    expect(React.createElement.mock.calls[0][1]).toBe(mockProps);
+
+    expect(React.createElement.mock.calls[0][0]).toBe(mockReactClass);
+    expect(React.createElement.mock.calls[0][1]).toBe(null);
+    expect(React.createElement.mock.calls[0][2]).toBe('inner child');
+
+    expect(React.createElement.mock.calls[0][0]).toBe(mockReactClass);
+    expect(React.createElement.mock.calls[0][1]).toBe(mockProps);
+    expect(React.createElement.mock.calls[0][2]).toBe('inner child');
+  });
+
+  it('should invoke React.createElement with an element with a child element', function () {
+    React.createElement.mockReturnValueOnce(mockReturn);
+
+    r.compose(
+      r('ol'),
+        r('li', mockProps),'list item', r('/li'),
+      r('/ol')
+    );
+
+    expect(React.createElement.mock.calls[0][0]).toBe('li');
+    expect(React.createElement.mock.calls[0][1]).toBe(mockProps);
+    expect(React.createElement.mock.calls[0][2]).toBe('list item');
+    expect(React.createElement.mock.calls[1][0]).toBe('ol');
+    expect(React.createElement.mock.calls[1][1]).toBe(null);
+    expect(React.createElement.mock.calls[1][2]).toBe(mockReturn);
   });
 });
--- src/r.js
+++ src/r.js
@@ -1,21 +1,51 @@
 var React = require('react');
-module.exports = function (el) {
-  if (!el) {
-    throw new Error('element must be defined');
-  }
-  if (el[el.length - 1] === '/') {
-    el = el.substr(0, el.length - 1);
-    el = el.trim();
-  }
-  if (arguments.length === 1) {
-    return React.createElement(el, null);
-  } else {
-    var propsChildrenOrText = arguments[1];
-    if (typeof propsChildrenOrText === 'string') {
-      return React.createElement(el, null, propsChildrenOrText);
-    } else if (typeof propsChildrenOrText === 'object') {
-      return React.createElement(el, propsChildrenOrText);
-    }
+var invariant = require('react/lib/invariant');
+
+var INVALID_TAG_ERROR = 'tag: "%s" is not a valid closing tag';
+
+var checkIfValidClosingTag = function (tag, isSelfClosing) {
+  var trimmedTag = tag.trim();
+  if ((trimmedTag[trimmedTag.length - 1] !== '/' && isSelfClosing) || (trimmedTag[0] !== '/') && !isSelfClosing) {
+    invariant(INVALID_TAG_ERROR, tag);
   }
 };
 
+var r = function (el, propsOrClosingTag, elClosingTag) {
+  var props = null;
+  var closingTag;
+  if (typeof el === 'string') {
+    if (el[0] === '/') {
+      return {
+        element: null,
+        closingTag: el
+      };
+    }
+  }
+  if (typeof propsOrClosingTag === 'string') {
+    checkIfValidClosingTag(propsOrClosingTag, true);
+    closingTag = propsOrClosingTag;
+  } else {
+    props = propsOrClosingTag;
+    if (elClosingTag) {
+      checkIfValidClosingTag(elClosingTag, true);
+      closingTag = elClosingTag;
+    }
+  }
+  return {
+    element: el,
+    props: props,
+    closingTag: closingTag
+  };
+};
+
+r.compose = function () {
+  var firstArg = arguments[0];
+  if (typeof firstArg === 'string') {
+    return firstArg;
+  }
+  if (firstArg.el && firstArg.closingTag) {
+
+  }
+};
+
+module.exports = r;
