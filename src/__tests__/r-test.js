49627cabab7db5b351fde7dc88e31560e6f076cd
drabinowitz <dmitrirabinowitz+code@gmail.com>
2015-01-31 17:00:38 -0500

    Complete basic functionality of compose syntax.

--- src/__tests__/r-test.js
+++ src/__tests__/r-test.js
@@ -26,7 +26,7 @@ describe('r', function () {
   });
 
   it('should invoke React.createElement with an element with text', function () {
-    r,compose(
+    r.compose(
       r('h1'), 'hello, world', r('/h1')
     );
 
@@ -71,16 +71,16 @@ describe('r', function () {
     expect(React.createElement.mock.calls[0][0]).toBe(mockReactClass);
     expect(React.createElement.mock.calls[0][1]).toBe(null);
 
-    expect(React.createElement.mock.calls[0][0]).toBe(mockReactClass);
-    expect(React.createElement.mock.calls[0][1]).toBe(mockProps);
+    expect(React.createElement.mock.calls[1][0]).toBe(mockReactClass);
+    expect(React.createElement.mock.calls[1][1]).toBe(mockProps);
 
-    expect(React.createElement.mock.calls[0][0]).toBe(mockReactClass);
-    expect(React.createElement.mock.calls[0][1]).toBe(null);
-    expect(React.createElement.mock.calls[0][2]).toBe('inner child');
+    expect(React.createElement.mock.calls[2][0]).toBe(mockReactClass);
+    expect(React.createElement.mock.calls[2][1]).toBe(null);
+    expect(React.createElement.mock.calls[2][2]).toBe('inner child');
 
-    expect(React.createElement.mock.calls[0][0]).toBe(mockReactClass);
-    expect(React.createElement.mock.calls[0][1]).toBe(mockProps);
-    expect(React.createElement.mock.calls[0][2]).toBe('inner child');
+    expect(React.createElement.mock.calls[3][0]).toBe(mockReactClass);
+    expect(React.createElement.mock.calls[3][1]).toBe(mockProps);
+    expect(React.createElement.mock.calls[3][2]).toBe('inner child');
   });
 
   it('should invoke React.createElement with an element with a child element', function () {
@@ -98,5 +98,46 @@ describe('r', function () {
     expect(React.createElement.mock.calls[1][0]).toBe('ol');
     expect(React.createElement.mock.calls[1][1]).toBe(null);
     expect(React.createElement.mock.calls[1][2]).toBe(mockReturn);
+  });
+
+  it('should invoke React.createElement with a complex element hierarchy', function () {
+    React.createElement.mockReturnValue(mockReturn);
+
+    r.compose(
+      r('div'),
+        'some text',
+        r('ol'),
+          r('li', mockProps), 'list item', r('/li'),
+          r(mockReactClass, mockProps, '/'),
+        r('/ol'),
+        r('h1'), r('span'), 'some text', r('/span'), r('/h1'),
+      r('/div')
+    );
+
+    expect(React.createElement.mock.calls[0][0]).toBe('li');
+    expect(React.createElement.mock.calls[0][1]).toBe(mockProps);
+    expect(React.createElement.mock.calls[0][2]).toBe('list item');
+
+    expect(React.createElement.mock.calls[1][0]).toBe(mockReactClass);
+    expect(React.createElement.mock.calls[1][1]).toBe(mockProps);
+
+    expect(React.createElement.mock.calls[2][0]).toBe('ol');
+    expect(React.createElement.mock.calls[2][1]).toBe(null);
+    expect(React.createElement.mock.calls[2][2]).toBe(mockReturn);
+    expect(React.createElement.mock.calls[2][3]).toBe(mockReturn);
+
+    expect(React.createElement.mock.calls[3][0]).toBe('span');
+    expect(React.createElement.mock.calls[3][1]).toBe(null);
+    expect(React.createElement.mock.calls[3][2]).toBe('some text');
+
+    expect(React.createElement.mock.calls[4][0]).toBe('h1');
+    expect(React.createElement.mock.calls[4][1]).toBe(null);
+    expect(React.createElement.mock.calls[4][2]).toBe(mockReturn);
+
+    expect(React.createElement.mock.calls[5][0]).toBe('div');
+    expect(React.createElement.mock.calls[5][1]).toBe(null);
+    expect(React.createElement.mock.calls[5][2]).toBe('some text');
+    expect(React.createElement.mock.calls[5][3]).toBe(mockReturn);
+    expect(React.createElement.mock.calls[5][4]).toBe(mockReturn);
   });
 });
--- src/r.js
+++ src/r.js
@@ -1,51 +1,114 @@
 var React = require('react');
 var invariant = require('react/lib/invariant');
 
-var INVALID_TAG_ERROR = 'tag: "%s" is not a valid closing tag';
+var INVALID_CLOSING_TAG = 'tag: "%s" is not a valid closing tag';
+var INVALID_TAG = 'tag: "%s" is not a valid tag';
+var INVALID_NUMBER_OF_ARGUMENTS = 'there are multiple outermost r.compose tags. r.compose needs to return a single outermost tag same as with the normal React class render method return';
+var INVALID_CLOSING_TAG_COUNT = 'There are not enough closing tags for this render composition';
 
 var checkIfValidClosingTag = function (tag, isSelfClosing) {
   var trimmedTag = tag.trim();
   if ((trimmedTag[trimmedTag.length - 1] !== '/' && isSelfClosing) || (trimmedTag[0] !== '/') && !isSelfClosing) {
-    invariant(INVALID_TAG_ERROR, tag);
+    invariant(INVALID_CLOSING_TAG, tag);
   }
 };
 
 var r = function (el, propsOrClosingTag, elClosingTag) {
   var props = null;
   var closingTag;
-  if (typeof el === 'string') {
-    if (el[0] === '/') {
-      return {
-        element: null,
-        closingTag: el
-      };
-    }
-  }
+  var trimmedEl;
+
   if (typeof propsOrClosingTag === 'string') {
     checkIfValidClosingTag(propsOrClosingTag, true);
     closingTag = propsOrClosingTag;
-  } else {
+  } else if (propsOrClosingTag && typeof propsOrClosingTag === 'object') {
     props = propsOrClosingTag;
     if (elClosingTag) {
       checkIfValidClosingTag(elClosingTag, true);
       closingTag = elClosingTag;
     }
   }
+
+  if (typeof el === 'string') {
+    trimmedEl = el.trim();
+    if (trimmedEl[0] === '/') {
+      return {
+        el: null,
+        closingTag: el
+      };
+    } else if (trimmedEl[trimmedEl.length - 1] === '/') {
+      closingTag = el;
+      el = trimmedEl.substr(0, trimmedEl.length - 1).trim();
+    }
+  }
+
   return {
-    element: el,
+    el: el,
     props: props,
     closingTag: closingTag
   };
 };
 
-r.compose = function () {
-  var firstArg = arguments[0];
-  if (typeof firstArg === 'string') {
-    return firstArg;
-  }
-  if (firstArg.el && firstArg.closingTag) {
+var isOutermostInvocation = true;
+var currentIndex = 0;
+var reactElementToReturn = null;
 
+var composeCleanup = function (argumentsLength) {
+  invariant(currentIndex === argumentsLength, INVALID_NUMBER_OF_ARGUMENTS);
+  isOutermostInvocation = true;
+  currentIndex = 0;
+  reactElementToReturn = null;
+};
+
+r.compose = function () {
+  var setOutermostInvocation = false;
+  if (isOutermostInvocation) {
+    setOutermostInvocation = true;
+    isOutermostInvocation = false;
+
+  } else {
+    children = [];
   }
+
+  var children;
+  var argumentsLength = arguments.length;
+  var reactElement;
+  var elementOrString;
+  while (currentIndex < argumentsLength) {
+    elementOrString = arguments[currentIndex];
+    currentIndex++;
+
+    if (typeof elementOrString === 'string') {
+
+      invariant(!setOutermostInvocation, INVALID_TAG, elementOrString);
+      children.push(elementOrString);
+
+    } else if (elementOrString.el && elementOrString.closingTag) {
+
+      reactElement = React.createElement(elementOrString.el, elementOrString.props);
+      if (setOutermostInvocation) {
+        composeCleanup(argumentsLength);
+        return reactElement;
+      } else {
+        children.push(reactElement);
+      }
+
+    } else if (elementOrString.el) {
+
+      reactElement = React.createElement.apply(React, [].concat(elementOrString.el, elementOrString.props, r.compose.apply(r, arguments)));
+      if (setOutermostInvocation) {
+        invariant(arguments[currentIndex - 1].closingTag, INVALID_CLOSING_TAG_COUNT);
+        composeCleanup(argumentsLength);
+        return reactElement;
+      } else {
+        children.push(reactElement);
+      }
+
+    } else if (elementOrString.closingTag) {
+      return children;
+    }
+  }
+  invariant(false, INVALID_CLOSING_TAG_COUNT);
 };
 
 module.exports = r;
